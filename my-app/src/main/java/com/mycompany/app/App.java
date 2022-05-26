93b935112d810effc2c1113c5812cd0ed8716e7b
HenryWotti <henrycky_wottikosky@hotmail.com>
2022-05-26 16:02:56 -0300

    Hello com lalyos

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,4 +1,7 @@
 package com.mycompany.app;
+import com.github.lalyos.jfiglet.FigletFont;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Hello world!
@@ -6,8 +9,29 @@ package com.mycompany.app;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
+        // using default font standard.flf, obtained from maven artifact
+        String asciiArt1 = FigletFont.convertOneLine("hello");
+        System.out.println(asciiArt1);
+        
+        // using font font2.flf, located somewhere in classpath under path /flf/font2.flf
+        String asciiArt2 = FigletFont.convertOneLine(FigletFont.class.getResourceAsStream("/flf/font2.flf"), "hello");
+        System.out.println(asciiArt2);
+        
+        asciiArt2 = FigletFont.convertOneLine("classpath:/flf/font2.flf", "hello");     
+        System.out.println(asciiArt2);                
+        
+        // using font font3.flf, located in file system under path /opt/font3.flf
+        String asciiArt3 = FigletFont.convertOneLine(new File("/opt/font3.flf"), "hello");     
+        System.out.println(asciiArt3);
+
+        asciiArt3 = FigletFont.convertOneLine("/opt/font3.flf", "hello");     
+        System.out.println(asciiArt3);
+
+        // using font font4.flf, from www 
+        String asciiArt4 = FigletFont.convertOneLine("http://myhost.com/font4.flf", "hello");     
+        System.out.println(asciiArt4); 
     }
 }
